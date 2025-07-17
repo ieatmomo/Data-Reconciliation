82cc9a4ab18919f26db51604b9f3b48e30d4cbcf
ieatmomo <deeveshrai125@gmail.com>
2025-07-17 11:18:21 +0100

    First Endpoint Added, Upload Files

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask,request, jsonify
+from models import AnalysisResult
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Welcome to the Flask App!"
 
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    # Check if both files are present
+    if 'old' not in request.files or 'new' not in request.files:
+        return jsonify({"error": "Missing one or more required files"}), 400
 
+    fileOld = request.files['old']
+    fileNew= request.files['new']
+
+    result = analyse_files(fileOld, fileNew)  # Custom function to process files
+    AnalysisResult.save_to_db(result)
+
+    return jsonify({"message": "Files received successfully!"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
